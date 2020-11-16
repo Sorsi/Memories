accc6865bfcb8760968994bd59e4342b319ef137
Orsolya <orsolya.suto@ericsson.com>
2020-11-16 20:08:06 +0100

    adding dotenv to the project

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({
     limit: "30mb",
@@ -20,10 +21,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://sutoorsi:sutoorsi@cluster0.8qayr.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {
+mongoose.connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((error) => console.log(error.message));
